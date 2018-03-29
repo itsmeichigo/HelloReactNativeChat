ab78b074ef13ba0b7e0f4fa198487e93f8738e20
Huong Do <ichigochan91@gmail.com>
2018-03-29 11:04:59 +0700

    Initial commit

---
+++ CustomChat/MessageMath.js
@@ -0,0 +1,82 @@
+/* eslint no-use-before-define: ["error", { "variables": false }] */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import { WebView, StyleSheet, View, ViewPropTypes, Text, Dimensions } from 'react-native';
+
+import AutoHeightWebView from 'react-native-autoheight-webview';
+
+export default class MessageMath extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  wrapMathjax(content) {
+
+		const mathOptions = {
+      showMathMenu: false,
+			messageStyle: 'none',
+			displayAlign: 'left',
+      tex2jax: {preview: 'none'},
+      ml2jax: {preview: 'none'}
+		};
+
+		return `
+      <html>
+        <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+          <script type="text/x-mathjax-config">
+             MathJax.Hub.Config(${mathOptions});
+           </script>
+           <script src="MathJax/MathJax.js?config=MML_SVG"></script>
+           <style type="text/css">
+            body { display: block; zoom: 1.0; -webkit-text-size-adjust: 100%%; }
+            div {display: table; vertical-align: middle;}
+           </style>
+         </head>
+         <body>
+          <div id=content>
+           <mathml style="font-size: 15px;"><math xmlns="http://www.w3.org/1998/Math/MathML" xmlns:mathematica="http://www.wolfram.com/XML/" mathematica:form="StandardForm">
+            ${content}
+            </math></mathml>
+          </div>
+        </body>
+      </html>
+    `;
+	}
+
+  render() {
+    if (this.props.currentMessage.content) {
+      return (
+        <View style={[styles.container, this.props.containerStyle]}>
+          <AutoHeightWebView
+            style={styles.web}
+            source={{html:this.wrapMathjax(this.props.currentMessage.content)}}
+          />
+        </View>
+      );
+    }
+    return null;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+  },
+  web: {
+    borderRadius: 13,
+    maxWidth: Dimensions.get('window').width - 100,
+  },
+});
+
+MessageMath.defaultProps = {
+  currentMessage: {
+    content: null,
+  },
+  containerStyle: {},
+};
+
+MessageMath.propTypes = {
+  currentMessage: PropTypes.object,
+  containerStyle: ViewPropTypes.style,
+};
---
+++ data/messages.js
@@ -0,0 +1,46 @@
+module.exports = [
+  {
+    _id: Math.round(Math.random() * 1000000),
+    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+    user: {
+      _id: 2,
+      name: 'React Native',
+    },
+    content: '<mtable displaystyle=\"true\"><mtr xmlns=\"http://www.w3.org/1998/Math/MathML\" xmlns:mathematica=\"http://www.wolfram.com/XML/\"> <mtd>  <mtable displaystyle=\"true\" columnalign=\"left\" indentalign=\"left\"><mtr> <mtd>  <mrow><mrow> <mtext>&nbsp;Add &nbsp;</mtext> <mn>4</mn> <mtext>&nbsp; to both sides&nbsp;</mtext></mrow><mtext></mtext>  </mrow> </mtd></mtr><mtr> <mtd>  <mrow><mrow> <msup>  <mi>x</mi>  <mn>2</mn> </msup> <mo>+</mo> <mrow>  <mn>4</mn>  <mo>&#8290;</mo>  <mi>x</mi> </mrow> <mo>+</mo> <mn>4</mn></mrow><mo>=</mo><mn>7</mn>  </mrow> </mtd></mtr>  </mtable> </mtd></mtr></mtable>'
+  },
+  {
+    _id: Math.round(Math.random() * 1000000),
+    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+    user: {
+      _id: 2,
+      name: 'React Native',
+    },
+    content: '<mtable displaystyle=\"true\"><mtr xmlns=\"http://www.w3.org/1998/Math/MathML\" xmlns:mathematica=\"http://www.wolfram.com/XML/\"> <mtd>  <mtable displaystyle=\"true\" columnalign=\"left\" indentalign=\"left\"><mtr> <mtd>  <mrow><mrow> <mrow>  <mtext>&nbsp;Solve for &nbsp;</mtext>  <mi>x</mi>  <mtext>&nbsp; over the real numbers&nbsp;</mtext> </mrow></mrow><mtext></mtext>  </mrow> </mtd></mtr><mtr> <mtd>  <mrow><mrow> <mrow>  <msup><mi>x</mi><mn>2</mn>  </msup>  <mo>+</mo>  <mrow><mn>4</mn><mo>&#8290;</mo><mi>x</mi>  </mrow>  <mo>-</mo>  <mn>3</mn> </mrow> <mo>=</mo> <mn>0</mn></mrow>  </mrow> </mtd></mtr>  </mtable> </mtd></mtr></mtable>'
+  },
+  {
+    _id: Math.round(Math.random() * 1000000),
+    text: 'Are you building a cat app?',
+    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+    user: {
+      _id: 2,
+      name: 'React Native',
+    },
+  },
+  {
+    _id: Math.round(Math.random() * 1000000),
+    image: 'https://pbs.twimg.com/media/DZTYL7dVMAEmbF7.jpg:large',
+    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+    user: {
+      _id: 1,
+      name: 'Developer',
+    },
+    sent: true,
+    received: true,
+  },
+  {
+    _id: Math.round(Math.random() * 1000000),
+    text: "You are officially rocking GiftedChat.",
+    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+    system: true,
+  },
+];
